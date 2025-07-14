41a7c7babd2c3feffa5fba1e933171dac38ddfb6
sukumar nayka <166438231+snayka@users.noreply.github.com>
2025-07-14 11:57:14 +0530

    Add files via upload

---
+++ Day_2/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { StudentFormComponent } from './components/student-form/student-form.component';
+
+export const routes: Routes = [
+    {path:'',component:StudentFormComponent}
+];
+ 
